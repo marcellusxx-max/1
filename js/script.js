8a7e8395cbfc185af5b8e90450159cc45ca288e3
marcellusxx-max <marcellusxx@gmail.com>
2025-11-15 19:41:34 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function testJS() {
+    document.getElementById('result').innerHTML = 
+        '<p style="color: green; font-weight: bold;">✅ JavaScript работает!</p>' +
+        '<p>Теперь можно добавлять основную форму</p>';
+}
+
+console.log("✅ Script.js загружен!");
